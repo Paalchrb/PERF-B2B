003ec70583cc79b7b92f0c1f81e03c7b26bda567
Pål-Christian By <Pal.christian.by@gmail.com>
2020-01-07 09:00:41 +0100

    fixed issues on heroku

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -1,1 +1,3 @@
 // add constants here
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 //import reducers here
+import alert from './alert';
 
 export default combineReducers({
   //export reducers here
+  alert,
 })
