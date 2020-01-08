7443b46b0e1c333bee9906f9fb9b52c63bbacb76
espensor <espen@sorby.net>
2020-01-08 16:59:43 +0100

    Added incomplete orders.js to router/api + added app.use for orders in server

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use('/', (req, res, next) => {
 // Define Routes
 app.use('/api/test', require('./routes/api/test'));
 app.use('/api/company', require('./routes/api/company'));
+app.use('/api/orders', require('./routes/api/orders'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
