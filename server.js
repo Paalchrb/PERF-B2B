38411065dd032e8a86f5c78737d4b1d09db97f93
Pål-Christian By <Pal.christian.by@gmail.com>
2020-01-08 16:51:54 +0100

    added auth middleware and started writing auth api

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use('/', (req, res, next) => {
 })
 
 // Define Routes
-app.use('/api/test', require('./routes/api/test'));
-app.use('/api/company', require('./routes/api/company'));
+app.use('/api/companies', require('./routes/api/companies'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
