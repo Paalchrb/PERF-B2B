be8c8da2e79f34eebd75a84e844fad7dbb18c690
RobinRF <robinfiske87@gmail.com>
2020-01-10 10:48:30 +0100

    link to views - navbar & order

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      navbar
+      <Link to='/'>
+        Landingpage
+      </Link>
+      <Link to='/dashboard'>
+        Dashboard
+      </Link>
+      <Link to='/login'>
+        Login
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar;
+
+
+
+
+
+
